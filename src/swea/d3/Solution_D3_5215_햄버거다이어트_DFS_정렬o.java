ec3e6b836a4c6a43c4989438713f38ce034799eb
GulliverNam <gullivernam94@gmail.com>
2020-02-10 23:51:55 +0900

    date: 02/10

--- src/swea/d3/Solution_D3_5215_햄버거다이어트_DFS_정렬o.java
+++ src/swea/d3/Solution_D3_5215_햄버거다이어트_DFS_정렬o.java
@@ -3,10 +3,13 @@ package swea.d3;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class Solution_D3_5215_햄버거다이어트_DFS {
-	static int N, meals[][];
+public class Solution_D3_5215_햄버거다이어트_DFS_정렬o {
+	static Integer meals[][];
+	static int N;
 	static int maxCal, answer;
 	public static void main(String args[]) throws Exception{
 		System.setIn(new FileInputStream("res/swea/d3/Solution_D3_5215_햄버거다이어트.txt"));
@@ -17,13 +20,21 @@ public class Solution_D3_5215_햄버거다이어트_DFS {
 			st = new StringTokenizer(br.readLine().trim(), " ");
 			N = Integer.parseInt(st.nextToken());
 			maxCal = Integer.parseInt(st.nextToken());
-			meals = new int[N][2];
+			meals = new Integer[N][2];
 			answer = 0;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine().trim(), " ");
 				meals[i][0] = Integer.parseInt(st.nextToken());
 				meals[i][1] = Integer.parseInt(st.nextToken());
 			}
+			Arrays.sort(meals, new Comparator<Integer[]>() {
+				@Override
+				public int compare(Integer[] o1, Integer[] o2) {
+					if(o1[1] == o2[1])
+						return o1[0] < o2[0] ? 1 : -1;
+					return o1[1] > o2[1] ? 1 : -1;
+				}
+			});
 			dfs(0, 0, 0);
 			System.out.println("#"+test_case+" "+answer);
 		}
@@ -33,6 +44,8 @@ public class Solution_D3_5215_햄버거다이어트_DFS {
 		for (int i = start; i < N; i++) {
 			if(sumCal+meals[i][1] <= maxCal)
 				dfs(i+1, sumCal+meals[i][1], sumScore+meals[i][0]);
+			else
+				return;
 		}
 	}
 }
--- src/swea/test/Solution_5653_줄기세포배양.java
+++ src/swea/test/Solution_5653_줄기세포배양.java
@@ -3,6 +3,7 @@ package swea.test;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -19,7 +20,7 @@ public class Solution_5653_줄기세포배양 {
 		int T = Integer.parseInt(br.readLine().trim());
 		int N, M, K;
 		int rStart, cStart;
-		for (int testCase = 1; testCase <= T; testCase++) {
+		for (int testCase = 1; testCase <= 5; testCase++) {
 			map = new int[1000][1000];
 			st = new StringTokenizer(br.readLine().trim(), " ");
 			N = Integer.parseInt(st.nextToken());
@@ -31,46 +32,86 @@ public class Solution_5653_줄기세포배양 {
 				st = new StringTokenizer(br.readLine().trim(), " ");
 				for (int j = cStart; j < cStart+M; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					Queue.add(new int[] {i,j,map[i][j], 0, 0});
-					// i 좌표, j 좌표, lifeTime, k(상태변화 or 배양 시점), 상태(0:비활성, 1:활성, 2:죽음)
+					if(map[i][j] != 0)
+						Queue.add(new int[] {i,j,map[i][j], 0, 0});
+					// i 좌표, j 좌표, lifeTime, k(상태변화 or 배양 시점), 상태(0:비활성, 1:활성, 2:활성 후 비활성)
 				}
 			}
 			for (int time = 1; time <= K; time++) {
-				curlture();
+				Queue = culture(time);
 			}
+			System.out.println("#"+testCase+" "+Queue.size());
 			Queue.clear();
 			
 		}
 	}
-	private static void curlture() {
+	private static LinkedList<int[]> culture(int time) {
 		LinkedList<int[]> temp = new LinkedList<>();
 		int length = Queue.size();
-		int current[], ci, cj, life, k, status, ni, nj;
-		for (int time = 0; time < length; time++) {
-			current = Queue.poll();
-			ci = current[0];
-			cj = current[1];
-			life = current[2];
-			k = current[3];
-			status = current[4];
+		int cur[], ci, cj, life, k, status, ni, nj;
+		for (int i = 0; i < length; i++) {
+			cur = Queue.poll();
+			ci = cur[0];
+			cj = cur[1];
+			life = cur[2];
+			k = cur[3];
+			status = cur[4];
 			switch(status) {
 			case 0:
 				if(life+k == time) {
-					k = k+time;
+					k = time;
 					status = 1;
 				}
 				temp.add(new int[] {ci, cj, life, k, status});
 				break;
 			case 1:
-				for (int i = 0; i < 4; i++) {
-					ni = ci + dir[i][0];
-					nj = cj + dir[i][1];
+				for (int j = 0; j < 4; j++) {
+					ni = ci + dir[j][0];
+					nj = cj + dir[j][1];
+					if(map[ni][nj] == 0) {
+						temp.add(new int[] {ni, nj, life, time, 0});
+					}
 				}
+				if(life == 1) break;
+				temp.add(new int[] {ci, cj, life, k, 2});
 				break;
 			case 2:
+				if(life+k == time) break;
+				temp.add(new int[] {ci, cj, life, k, status});
 				break;
 			}
 		}
+		temp.sort(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0] == o2[0] && o1[1] == o2[1])
+					return o1[2] > o2[2] ? 1 : -1;
+				if(o1[0] == o2[0])
+					return o1[1] > o2[1] ? 1 : -1;
+				return o1[0] > o2[0] ? 1 : -1;
+			}
+		});
+		int prev[], remove[] = new int[temp.size()];
+		int idx = 0;
+		for (int i = 0; i < temp.size(); i++) {
+			cur = temp.get(i);
+			if(cur[4] == 0 && cur[3] == time) {
+				if(map[cur[0]][cur[1]] == 0)
+					map[cur[0]][cur[1]] = cur[2];
+				else {
+					prev = temp.get(i-1);
+					if(prev[0] == cur[0] && prev[1] == cur[1]) {
+						map[cur[0]][cur[1]] = cur[2];
+						remove[idx++] = i-1;
+					}
+				}
+			}
+		}
+		for (int i = idx-1; i > -1; i--) {
+			temp.remove(remove[i]);
+		}
+		
+		return temp;
 		
 	}
 
